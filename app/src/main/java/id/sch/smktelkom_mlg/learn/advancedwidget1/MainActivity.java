820c302b2cef525d579c770c00df584038619184
Vergie Ericson T <vergieericsont@gmail.com>
2016-11-03 00:28:49 +0700

    Widget Programming Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
@@ -1,7 +1,13 @@
 package id.sch.smktelkom_mlg.learn.advancedwidget1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout llMain = (LinearLayout) findViewById(R.id.linearLayoutMain);
+        final EditText etNama = new EditText(this);
+        llMain.addView(etNama);
+        etNama.setHint("Isikan Nama Anak");
+
+        final EditText etUmur = new EditText(this);
+        llMain.addView(etUmur);
+        etUmur.setHint("Isikan Umur Anak");
+        etUmur.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        Button btnProses = new Button(this);
+        btnProses.setText("Proses");
+        llMain.addView(btnProses);
+        final TextView tvHasil = new TextView(this);
+        llMain.addView(tvHasil);
+        btnProses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama = etNama.getText().toString();
+                String umur = etUmur.getText().toString();
+
+                tvHasil.setText(nama + " umur " + umur + " tahun");
+            }
+        });
     }
 }
